c7b6bd27536c06a1c8655606ca2e95a4b4c58b07
masciotto <giosue.deriu@gmail.com>
2026-09-10 15:55:36 +0200

    Update database.js

--- backend/src/db/database.js
+++ backend/src/db/database.js
@@ -7,8 +7,12 @@ const pool = new Pool({
   }
 });
 
+// Funzione di aiuto per le query
 async function query(text, params) {
+  const start = Date.now();
   const res = await pool.query(text, params);
+  const duration = Date.now() - start;
+  console.log("executed query", { text, duration, rows: res.rowCount });
   return res;
 }
 
